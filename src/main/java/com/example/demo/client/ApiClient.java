bf7c0e8febe35402ba09475a608a1c845aa1c9a8
RICHARD WILLIAM <richard.william@muf.co.id>
2025-03-11 09:36:25 +0700

    feat: implemented raven-client

--- src/main/java/com/example/demo/client/ApiClient.java
+++ src/main/java/com/example/demo/client/ApiClient.java
@@ -1,5 +1,6 @@
 package com.example.demo.client;
 
+import com.nephren.raven.apiclient.annotation.RavenApiClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import reactor.core.publisher.Mono;
 
-@com.blibli.oss.backend.apiclient.annotation.ApiClient(
+@RavenApiClient(
     name = "apiClient"
 )
 public interface ApiClient {
